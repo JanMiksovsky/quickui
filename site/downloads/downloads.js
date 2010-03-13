b915af4ee880698331f752664b12e4d11764bafe
JanMiksovsky <jan@miksovsky.com>
2010-03-13 06:28:56 +0000

    Apply new <content> vs <prototype> distinction to site pages and common controls.

--- site/downloads/downloads.js
+++ site/downloads/downloads.js
@@ -1,60 +1,54 @@
 //
 // DownloadPage
 //
-DownloadPage = QuickUI.Control.extend({
+DownloadPage = SitePage.extend({
 	className: "DownloadPage",
 	render: function() {
-		QuickUI.Control.prototype.render.call(this);
-		this.setClassProperties(QuickUI.Control, {
+		SitePage.prototype.render.call(this);
+		this.setClassProperties(SitePage, {
+			"area": "Download",
+			"title": "Download QuickUI",
 			"content": [
+				"\n\t\n",
+				"<h2>Windows</h2>",
 				"\n\n",
-				QuickUI.Control.create(SitePage, {
-					"content": [
-						"\n\t\n",
-						"<h2>Windows</h2>",
-						"\n\n",
-						$("<ol />").items(
-							"\n\t",
-							$("<li />").items(
-								QuickUI.Control.create(Link, {
-									"content": "Download the installer",
-									"href": "/downloads/QuickUI%20Setup.msi"
-								}),
-								"."
-							)[0],
-							"\n\t",
-							"<li>Add the QuickUI tools folder (e.g., C:\\Program Files\\QuickUI\\bin) to your path.</li>",
-							"\n"
-						)[0],
-						"\n\n",
-						"<div style=\"color: lightgray\">\r\n<h2>Mac OS/X</h2>\r\n<ol>\r\n\t<li>Download Mono</li>\r\n\t<li>Download package</li>\r\n\t<li>Add the QuickUI shell folder to your path.</li>\r\n</ol>\r\n</div>",
-						"\n\n",
-						$("<p />").items(
-							"After downloading, you can run through the quick ",
-							QuickUI.Control.create(Link, {
-								"content": "tutorial",
-								"href": "/tutorial/section01/default.html"
-							}),
-							"."
-						)[0],
-						"\n\n",
-						"<h1>Integrate QuickUI into a development environment</h1>",
-						"\n\n",
-						"<p>\r\nQuick markup files use the .qui extension. You can associate this extension with your\r\nprefered XML or HTML editor. (An HTML editor will likely complain about some unknown elements,\r\nbut you’ll pick up syntax highlighting, auto-completion, etc., for CSS rules and JavaScript\r\nembedded in the Quick markup.)\r\n</p>",
-						"\n\n",
-						"<h2>Aptana Studio</h2>",
-						"\n\nYou can configure ",
+				$("<ol />").items(
+					"\n\t",
+					$("<li />").items(
 						QuickUI.Control.create(Link, {
-							"content": "Aptana Studio",
-							"href": "http://www.aptana.com/studio"
+							"content": "Download the installer",
+							"href": "/downloads/QuickUI%20Setup.msi"
 						}),
-						" to \nautomatically build a QuickUI project by invoking the qb tool.\n\n",
-						"<ol>\r\n\t<li>Create or open a project in Aptana Studio.</li>\r\n\t<li>From the Project menu, choose Properties.</li>\r\n\t<li>Select the Builders tab.</li>\r\n\t<li>First you’ll create a Builder to build your Aptana project using qb. Click the New… button.</li>\r\n\t<li>Select “Program”.</li>\r\n\t<li>Give the builder a name like “qb”.</li>\r\n\t<li>On the Main tab, enter a Location of “${system_path:qb.exe}”. For the Working Directory, use the Browse Workspace… button to select your project.</li>\r\n\t<li>On the Refresh tab, check “Refresh resources upon completion”, and “The project containing the selected resource”.</li>\r\n\t<li>Click OK to save the Builder.</li>\r\n\t<li>Next you’ll create a Builder to clean your Aptana project using qb with the “-clean” option. Click the New… button again.</li>\r\n\t<li>Select “Program”.</li>\r\n\t<li>Give the builder a name like “qb -clean”.</li>\r\n\t<li>On the Main tab, in the Arguments text box, enter “-clean”. Enter the same Location (${system_path:qb.exe}) and Working Directory. </li>\r\n\t<li>On the Refresh tab, check “Refresh resources upon completion”, and “The project containing the selected resource”.</li>\r\n\t<li>On the Build Options tab, check “During a ‘Clean’”, and uncheck the other “Run the builder” check boxes.</li>\r\n\t<li>Click OK to save the Builder.</li>\r\n\t<li>Click OK to close the Properties dialog.</li>\r\n\t<li>From the Project menu, select “Build Project”. You should see qb output in the Console view.</li>\r\n</ol>",
-						"\n\n"
-					],
-					"area": "Download",
-					"title": "Download QuickUI"
+						"."
+					)[0],
+					"\n\t",
+					"<li>Add the QuickUI tools folder (e.g., C:\\Program Files\\QuickUI\\bin) to your path.</li>",
+					"\n"
+				)[0],
+				"\n\n",
+				"<div style=\"color: lightgray\">\r\n<h2>Mac OS/X</h2>\r\n<ol>\r\n\t<li>Download Mono</li>\r\n\t<li>Download package</li>\r\n\t<li>Add the QuickUI shell folder to your path.</li>\r\n</ol>\r\n</div>",
+				"\n\n",
+				$("<p />").items(
+					"After downloading, you can run through the quick ",
+					QuickUI.Control.create(Link, {
+						"content": "tutorial",
+						"href": "/tutorial/section01/default.html"
+					}),
+					"."
+				)[0],
+				"\n\n",
+				"<h1>Integrate QuickUI into a development environment</h1>",
+				"\n\n",
+				"<p>\r\nQuick markup files use the .qui extension. You can associate this extension with your\r\nprefered XML or HTML editor. (An HTML editor will likely complain about some unknown elements,\r\nbut you’ll pick up syntax highlighting, auto-completion, etc., for CSS rules and JavaScript\r\nembedded in the Quick markup.)\r\n</p>",
+				"\n\n",
+				"<h2>Aptana Studio</h2>",
+				"\n\nYou can configure ",
+				QuickUI.Control.create(Link, {
+					"content": "Aptana Studio",
+					"href": "http://www.aptana.com/studio"
 				}),
+				" to \nautomatically build a QuickUI project by invoking the qb tool.\n\n",
+				"<ol>\r\n\t<li>Create or open a project in Aptana Studio.</li>\r\n\t<li>From the Project menu, choose Properties.</li>\r\n\t<li>Select the Builders tab.</li>\r\n\t<li>First you’ll create a Builder to build your Aptana project using qb. Click the New… button.</li>\r\n\t<li>Select “Program”.</li>\r\n\t<li>Give the builder a name like “qb”.</li>\r\n\t<li>On the Main tab, enter a Location of “${system_path:qb.exe}”. For the Working Directory, use the Browse Workspace… button to select your project.</li>\r\n\t<li>On the Refresh tab, check “Refresh resources upon completion”, and “The project containing the selected resource”.</li>\r\n\t<li>Click OK to save the Builder.</li>\r\n\t<li>Next you’ll create a Builder to clean your Aptana project using qb with the “-clean” option. Click the New… button again.</li>\r\n\t<li>Select “Program”.</li>\r\n\t<li>Give the builder a name like “qb -clean”.</li>\r\n\t<li>On the Main tab, in the Arguments text box, enter “-clean”. Enter the same Location (${system_path:qb.exe}) and Working Directory. </li>\r\n\t<li>On the Refresh tab, check “Refresh resources upon completion”, and “The project containing the selected resource”.</li>\r\n\t<li>On the Build Options tab, check “During a ‘Clean’”, and uncheck the other “Run the builder” check boxes.</li>\r\n\t<li>Click OK to save the Builder.</li>\r\n\t<li>Click OK to close the Properties dialog.</li>\r\n\t<li>From the Project menu, select “Build Project”. You should see qb output in the Console view.</li>\r\n</ol>",
 				"\n\n"
 			]
 		});
--- site/home/home.js
+++ site/home/home.js
@@ -49,116 +49,110 @@ Greet = QuickUI.Control.extend({
 //
 // HomePage
 //
-HomePage = QuickUI.Control.extend({
+HomePage = SitePage.extend({
 	className: "HomePage",
 	render: function() {
-		QuickUI.Control.prototype.render.call(this);
-		this.setClassProperties(QuickUI.Control, {
+		SitePage.prototype.render.call(this);
+		this.setClassProperties(SitePage, {
+			"area": "Home",
+			"title": "QuickUI modular web control framework",
+			"sidebar": [
+				"\n\t",
+				QuickUI.Control.create(GetStartedModule),
+				"\n"
+			],
 			"content": [
 				"\n\n",
-				QuickUI.Control.create(SitePage, {
+				"\n\n",
+				"<p>\r\nQuickUI (Quick User Interface) is a framework that simplifies the design, construction,\r\nand maintenance of web-based user interfaces. The easiest way to describe QuickUI is\r\nthat <i>it lets you write HTML as if you could create your own tags</i>.\r\n</p>",
+				"\n",
+				"<p>\t\r\nIn QuickUI, the tags you create are called controls. You define controls with declarative\r\nmarkup that can include HTML (including other QuickUI controls), CSS styling,\r\nand JavaScript scripting. You can easily create new controls by bundling together a group\r\nof existing controls, or by subclassing an existing control to make it more special-purpose.\r\nThe page you are reading here, and remainder of this site, is built entirely using QuickUI.\r\n</p>",
+				"\n",
+				$("<p />").items(
+					"\nQuickUI includes tools that compile your markup into regular JavaScript and CSS files.\nThese tools run on Windows and OS/X (via the Mono project). The generated JavaScript\nmakes use of the ",
+					QuickUI.Control.create(Link, {
+						"content": "jQuery",
+						"href": "http://jquery.com"
+					}),
+					" library to\nbuild the web user interface on demand. QuickUI also includes a run-time library.\nOverall QuickUI is targetted at and tested against the mainstream browsers:\nApple Safari 4.x, Google Chrome 3.x, Internet Explorer 8.x, and Mozilla Firefox 3.x.\n"
+				)[0],
+				"\n\n",
+				"<h2>A quick example</h2>",
+				"\n\n",
+				"<p>\r\n\tThe following Quick markup defines a simple control called “Greet”.\r\n</p>",
+				"\n\n",
+				QuickUI.Control.create(SourceCode, {
+					"sourceFile": "Greet.qui"
+				}),
+				"\n\n",
+				$("<p />").items(
+					"\nThis control definition gives you a new tag\n",
+					QuickUI.Control.create(Tag, {
+						"content": "Greet"
+					}),
+					" that can be used in other Quick markup like this:\n"
+				)[0],
+				"\n\n",
+				QuickUI.Control.create(SourceCode, {
+					"sourceFile": "Sample.qui"
+				}),
+				"\n\n",
+				"<p>\r\nKicking everything off is accomplished in JavaScript via a jQuery extension:\r\n</p>",
+				"\n\n",
+				"<pre>\r\n$(\"div\").control(Sample);\r\n</pre>",
+				"\n\n",
+				"<p>\r\nThe div below shows the actual sample control above instantiated here on this page:\r\n</p>",
+				"\n\n",
+				QuickUI.Control.create(CodeOutput, {
 					"content": [
-						"\n\n",
-						"\n\n",
-						"<p>\r\nQuickUI (Quick User Interface) is a framework that simplifies the design, construction,\r\nand maintenance of web-based user interfaces. The easiest way to describe QuickUI is\r\nthat <i>it lets you write HTML as if you could create your own tags</i>.\r\n</p>",
-						"\n",
-						"<p>\t\r\nIn QuickUI, the tags you create are called controls. You define controls with declarative\r\nmarkup that can include HTML (including other QuickUI controls), CSS styling,\r\nand JavaScript scripting. You can easily create new controls by bundling together a group\r\nof existing controls, or by subclassing an existing control to make it more special-purpose.\r\nThe page you are reading here, and remainder of this site, is built entirely using QuickUI.\r\n</p>",
-						"\n",
-						$("<p />").items(
-							"\nQuickUI includes tools that compile your markup into regular JavaScript and CSS files.\nThese tools run on Windows and OS/X (via the Mono project). The generated JavaScript\nmakes use of the ",
-							QuickUI.Control.create(Link, {
-								"content": "jQuery",
-								"href": "http://jquery.com"
-							}),
-							" library to\nbuild the web user interface on demand. QuickUI also includes a run-time library.\nOverall QuickUI is targetted at and tested against the mainstream browsers:\nApple Safari 4.x, Google Chrome 3.x, Internet Explorer 8.x, and Mozilla Firefox 3.x.\n"
-						)[0],
-						"\n\n",
-						"<h2>A quick example</h2>",
-						"\n\n",
-						"<p>\r\n\tThe following Quick markup defines a simple control called “Greet”.\r\n</p>",
-						"\n\n",
-						QuickUI.Control.create(SourceCode, {
-							"sourceFile": "Greet.qui"
-						}),
-						"\n\n",
-						$("<p />").items(
-							"\nThis control definition gives you a new tag\n",
-							QuickUI.Control.create(Tag, {
-								"content": "Greet"
-							}),
-							" that can be used in other Quick markup like this:\n"
-						)[0],
-						"\n\n",
-						QuickUI.Control.create(SourceCode, {
-							"sourceFile": "Sample.qui"
-						}),
-						"\n\n",
-						"<p>\r\nKicking everything off is accomplished in JavaScript via a jQuery extension:\r\n</p>",
-						"\n\n",
-						"<pre>\r\n$(\"div\").control(Sample);\r\n</pre>",
-						"\n\n",
-						"<p>\r\nThe div below shows the actual sample control above instantiated here on this page:\r\n</p>",
-						"\n\n",
-						QuickUI.Control.create(CodeOutput, {
-							"content": [
-								"\n\t",
-								QuickUI.Control.create(Sample),
-								"\n"
-							]
-						}),
-						"\n\n",
-						$("<p />").items(
-							"\nThis is a trivial example. The ",
-							QuickUI.Control.create(Link, {
-								"content": "tutorial",
-								"href": "/tutorial/section01/default.html"
-							}),
-							"\nexamines controls with complex contents, control styling, and interactive behavior.\nWhen combined with the ability to compose and subclass controls,\nthings can quickly become very interesting. You can see some sample QuickUI controls\nin the ",
-							QuickUI.Control.create(Link, {
-								"content": "Gallery",
-								"href": "/gallery/default.html"
-							}),
-							".\n"
-						)[0],
-						"\n\n",
-						"<h2>Why use QuickUI?</h2>",
-						"\n\n",
-						"<p>\r\nConsider using QuickUI because it:\r\n</p>",
-						"\n\n",
-						"<ul>\r\n\t<li>\r\n\t\tAllows you to create UI in a markup language that is easy to read,\r\n\t\tis easy to work with, and can concisely express your design intention.\r\n\t\tThis speeds up UI development, and makes UI code more maintainable.\r\n\t</li>\r\n\t<li>\r\n\t\tWorks with the web’s native technologies, it doesn’t replace them.\r\n\t\tQuickUI is true to HTML, CSS, and JavaScript. Familiar conventions are used\r\n\t\twhenever possible. QuickUI is only meant to address the web’s painful lack\r\n\t\tof an extensible control framework.\r\n\t</li>\r\n\t<li>\r\n\t\tAllows designers and developers to share design artifacts. QuickUI can bridge\r\n\t\tthe gap between pure visual design tools and native web coding frameworks.\r\n\t\tThe markup language can be easily learned by HTML-savvy designers, and is\r\n\t\tstill powerful and flexible enough to appeal to developers.\r\n\t</li>\r\n\t<li>\r\n\t\tEncourages well-factored UI code. Controls are as modular as possible,\r\n\t\teasily composable, and easily subclassable. All the goodness of object-oriented\r\n\t\tdesign can be applied to your UI code. Package up your hard-earned knowledge\r\n\t\tof HTML and CSS idiosyncrasies into reusable controls that can be readily\r\n\t\tapplied to new problems.\r\n\t</li>\r\n\t<li>\r\n\t\tProduces predictable compiled code. The QuickUI tools do a minimum of\r\n\t\tcompilation magic. The compiler’s output are legible, understandable,\r\n\t\treadily debuggable JavaScript classes. The generated code takes care of \r\n\t\tpopulating the control’s run-time DOM — code that is often tedious to write\r\n\t\tby hand, and normally hard to read. The behavior of these controls can\r\n\t\tbe programmed in JavaScript and jQuery.\r\n\t</li>\r\n\t<li>\r\n\t\tCreates controls that can be used as top-level pages on their own,\r\n\t\tor easily included in web pages built with other technologies.\r\n\t</li>\r\n\t<li>\r\n\t\tIs fun to work in!\r\n\t</li>\r\n</ul>",
-						"\n\nQuickUI is completely free, and the ",
-						QuickUI.Control.create(Link, {
-							"content": "source code",
-							"href": "http://code.google.com/p/quickui/"
-						}),
-						"\nis open under the ",
-						QuickUI.Control.create(Link, {
-							"content": "MIT License",
-							"href": "http://www.opensource.org/licenses/mit-license.php"
-						}),
-						".\n\n",
-						$("<p />").items(
-							"\n",
-							QuickUI.Control.create(Link, {
-								"content": "Download QuickUI",
-								"href": "/downloads/default.html"
-							}),
-							"\n"
-						)[0],
-						"\n\n",
-						"<br />",
-						"\n\n",
-						QuickUI.Control.create(AddThis),
-						"\n\n"
-					],
-					"area": "Home",
-					"title": "QuickUI modular web control framework",
-					"sidebar": [
 						"\n\t",
-						QuickUI.Control.create(GetStartedModule),
+						QuickUI.Control.create(Sample),
 						"\n"
 					]
 				}),
+				"\n\n",
+				$("<p />").items(
+					"\nThis is a trivial example. The ",
+					QuickUI.Control.create(Link, {
+						"content": "tutorial",
+						"href": "/tutorial/section01/default.html"
+					}),
+					"\nexamines controls with complex contents, control styling, and interactive behavior.\nWhen combined with the ability to compose and subclass controls,\nthings can quickly become very interesting. You can see some sample QuickUI controls\nin the ",
+					QuickUI.Control.create(Link, {
+						"content": "Gallery",
+						"href": "/gallery/default.html"
+					}),
+					".\n"
+				)[0],
+				"\n\n",
+				"<h2>Why use QuickUI?</h2>",
+				"\n\n",
+				"<p>\r\nConsider using QuickUI because it:\r\n</p>",
+				"\n\n",
+				"<ul>\r\n\t<li>\r\n\t\tAllows you to create UI in a markup language that is easy to read,\r\n\t\tis easy to work with, and can concisely express your design intention.\r\n\t\tThis speeds up UI development, and makes UI code more maintainable.\r\n\t</li>\r\n\t<li>\r\n\t\tWorks with the web’s native technologies, it doesn’t replace them.\r\n\t\tQuickUI is true to HTML, CSS, and JavaScript. Familiar conventions are used\r\n\t\twhenever possible. QuickUI is only meant to address the web’s painful lack\r\n\t\tof an extensible control framework.\r\n\t</li>\r\n\t<li>\r\n\t\tAllows designers and developers to share design artifacts. QuickUI can bridge\r\n\t\tthe gap between pure visual design tools and native web coding frameworks.\r\n\t\tThe markup language can be easily learned by HTML-savvy designers, and is\r\n\t\tstill powerful and flexible enough to appeal to developers.\r\n\t</li>\r\n\t<li>\r\n\t\tEncourages well-factored UI code. Controls are as modular as possible,\r\n\t\teasily composable, and easily subclassable. All the goodness of object-oriented\r\n\t\tdesign can be applied to your UI code. Package up your hard-earned knowledge\r\n\t\tof HTML and CSS idiosyncrasies into reusable controls that can be readily\r\n\t\tapplied to new problems.\r\n\t</li>\r\n\t<li>\r\n\t\tProduces predictable compiled code. The QuickUI tools do a minimum of\r\n\t\tcompilation magic. The compiler’s output are legible, understandable,\r\n\t\treadily debuggable JavaScript classes. The generated code takes care of \r\n\t\tpopulating the control’s run-time DOM — code that is often tedious to write\r\n\t\tby hand, and normally hard to read. The behavior of these controls can\r\n\t\tbe programmed in JavaScript and jQuery.\r\n\t</li>\r\n\t<li>\r\n\t\tCreates controls that can be used as top-level pages on their own,\r\n\t\tor easily included in web pages built with other technologies.\r\n\t</li>\r\n\t<li>\r\n\t\tIs fun to work in!\r\n\t</li>\r\n</ul>",
+				"\n\nQuickUI is completely free, and the ",
+				QuickUI.Control.create(Link, {
+					"content": "source code",
+					"href": "http://code.google.com/p/quickui/"
+				}),
+				"\nis open under the ",
+				QuickUI.Control.create(Link, {
+					"content": "MIT License",
+					"href": "http://www.opensource.org/licenses/mit-license.php"
+				}),
+				".\n\n",
+				$("<p />").items(
+					"\n",
+					QuickUI.Control.create(Link, {
+						"content": "Download QuickUI",
+						"href": "/downloads/default.html"
+					}),
+					"\n"
+				)[0],
+				"\n\n",
+				"<br />",
+				"\n\n",
+				QuickUI.Control.create(AddThis),
 				"\n\n"
 			]
 		});
