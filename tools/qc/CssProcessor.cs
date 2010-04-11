8a0c76dd23b72e6c76d6dd253c6a12bc3fbe907a
JanMiksovsky <jan@miksovsky.com>
2010-04-11 04:43:09 +0000

    Support comma-separated style selectors like "h1, h2 { color: red; }"

--- tools/qc/CssProcessor.cs
+++ tools/qc/CssProcessor.cs
@@ -28,12 +28,12 @@ namespace qc
                     new
                     {
                         ClassName = c.Name,
-                        Styles = AddClassNameToStyles(c.Name, styles)
+                        Styles = AddClassNameToSelectors(c.Name, styles)
                     });
         }
 
         /// <summary>
-        /// Prepend the class name to all style rules
+        /// Prepend the class name to all style selectors.
         /// </summary>
         /// <remarks>
         /// If the class name is "Foo", this will find all rules in the CSS like
@@ -42,63 +42,48 @@ namespace qc
         /// and avoids style name conflicts with styles defined by other controls
         /// in use on the same page.
         /// 
-        /// Exception: if the rule already begins with .Foo, then the the class name
+        /// Exception: if the selector already begins with .Foo, then the the class name
         /// is *not* prepended. This allows for the definition of rules like ".Foo.Bar".
         /// </remarks>
-        static string AddClassNameToStyles(string className, string styles)
+        static string AddClassNameToSelectors(string className, string styles)
         {
             // We could do a simple findClassNames.Replace() if it weren't for the
             // exception about not prepending the class name to the rule if the
             // class name is already there. So, we'll do a manual match-and-replace.
-            MatchCollection matches = findClassNames.Matches(styles);
+            MatchCollection matches = findSelectorsRegEx.Matches(styles);
 
-            // Walk backward through the matches so the match indexes stay relevant
-            // during the loop.
+            // Walk backward through the matches and captures so the match indexes
+            // stay valid during the loop.
             string cssClassName = "." + className;
             for (int i = matches.Count - 1; i >= 0; i--)
             {
-                Match match = matches[i];
-                Group cssRuleGroup = match.Groups[2];
-
-                // Ignore style rules that meet any of the following conditions:
-                //  1) the style name is already included,
-                //  2) the rule is a @font-face directive.
-                if (!cssRuleGroup.Value.StartsWith(cssClassName) &&
-                    !cssRuleGroup.Value.StartsWith("@font-face"))
+                CaptureCollection ruleCaptures = matches[i].Groups[1].Captures;
+                for (int j = ruleCaptures.Count - 1; j >= 0; j--)
                 {
-                    // CSS rule doesn't already start with class name, so prepend it.
-                    styles = styles.Insert(cssRuleGroup.Index, cssClassName + " ");
+                    Capture capture = ruleCaptures[j];
+
+                    // Ignore selectors that meet any of the following conditions:
+                    //  1) the style name is already included,
+                    //  2) the selector is a CSS directive (starts with "@").
+                    if (!capture.Value.StartsWith(cssClassName) &&
+                        !capture.Value.StartsWith("@"))
+                    {
+                        // Selector doesn't already start with class name, so prepend it.
+                        styles = styles.Insert(capture.Index, cssClassName + " ");
+                    }
                 }
             }
 
             return styles;
         }
 
-        // Regex to find class names in a style.
-        // This is in two parts:
-        //
-        // The first capture group finds white space and comments before a rule.
-        //      \s*(?:/\*.*?\*/\s*)*
-        //          Whitespace, any number of repetitions
-        //          Match expression but don't capture it. [/\*.*?\*/\s*], any number of repetitions
-        //              /\*.*?\*/\s*
-        //                  /
-        //                  Literal *
-        //                  Any character, any number of repetitions, as few as possible
-        //                  Literal *
-        //                  /
-        //                  Whitespace, any number of repetitions
-        //
-        // The second capture group finds the style classes/selectors and style definition.
-        // This is basically anything up to an ending curly brace. This regex will FAIL if
-        // someone puts a brace inside of a style definition (e.g., in a comment).
-        //      [^\{]*\{[^\}]*\}
-        //          Any character that is NOT in this class: [\{], any number of repetitions
-        //          Literal {
-        //          Any character that is NOT in this class: [\}], any number of repetitions
-        //          Literal }
-        //
-        static Regex findClassNames = new Regex(@"(\s*(?:/\*.*?\*/\s*)*)([^\{]*\{[^\}]*\})", RegexOptions.Compiled);
+        // Regex to find selectors in CSS.
+        private const string findSelectors = 
+            @"(?:/\*.*?\*/\s*)*" +              // Absorb comments
+            @"(?<selector>.*?)\s*" +            // First selector
+            @"(?:,\s*(?<selector>.*?)\s*)*" +   // Comma separated remaining selector
+            @"\{[^\}]*\}";                      // Style
+        private static Regex findSelectorsRegEx = new Regex(findSelectors, RegexOptions.Compiled);
 
 #if DEBUG
         [TestFixture]
@@ -119,15 +104,39 @@ namespace qc
             public void Basic()
             {
                 string styles = "h1 { font-weight: bold; }";
-                string result = AddClassNameToStyles("Foo", styles);
+                string result = AddClassNameToSelectors("Foo", styles);
                 Assert.AreEqual(".Foo h1 { font-weight: bold; }", result);
+            }
+
+            [Test]
+            public void RuleOnDifferentLine()
+            {
+                string styles = "h1\n{ font-weight: bold; }";
+                string result = AddClassNameToSelectors("Foo", styles);
+                Assert.AreEqual(".Foo h1\n{ font-weight: bold; }", result);
+            }
+
+            [Test]
+            public void Multiple()
+            {
+                string styles = "h1 { color: red; }\nh2 { color: orange; }";
+                string result = AddClassNameToSelectors("Foo", styles);
+                Assert.AreEqual(".Foo h1 { color: red; }\n.Foo h2 { color: orange; }", result);
+            }
+
+            [Test]
+            public void CommaSeparatedRules()
+            {
+                string styles = "h1, h2, h3 { font-weight: bold; }";
+                string result = AddClassNameToSelectors("Foo", styles);
+                Assert.AreEqual(".Foo h1, .Foo h2, .Foo h3 { font-weight: bold; }", result);
             }
 
             [Test]
             public void ClassNameAlreadyIncluded()
             {
                 string styles = ".Foo.h1 { font-weight: bold; }";
-                string result = AddClassNameToStyles("Foo", styles);
+                string result = AddClassNameToSelectors("Foo", styles);
                 Assert.AreEqual(".Foo.h1 { font-weight: bold; }", result);
             }
 
@@ -135,7 +144,7 @@ namespace qc
             public void FontFace()
             {
                 string styles = "@font-face { font-family: Arial; local('Arial'); }";
-                string result = AddClassNameToStyles("Foo", styles);
+                string result = AddClassNameToSelectors("Foo", styles);
                 Assert.AreEqual(styles, result);
             }
         }
